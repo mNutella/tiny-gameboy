6fd29bba01b3663df14397386feca14b4d1d952a
nutella <netcore.click@gmail.com>
2023-09-08 11:29:31 -0400

    init commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// TODO:
+// CPU(ALU, Registers, Instruction Address, Instruction Counter)
+// Memory(RAM)
+// Memory(ROM)
+// Graphic Memory(Matrix)
+// I/O(Screen, Controller)
+
+fn main() {
+    println!("Game Boy!");
+}
